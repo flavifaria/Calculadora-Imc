3d010f25a2754a820f5457a8953d0db749b98e15
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2025-10-20 12:23:28 -0300

    Atualização arquivos

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ function imc(){
 
     const valorIMC = (peso/(altura*altura)).toFixed(1);
 
-    resultado.textContent = `${nome} seu IMC é ${valorIMC}!`;
-}
+    let classificacao = "";
 
+    if (valorIMC < 18.5){
+        classificacao = "Abaixo do peso";
+    }else if(valorIMC < 25){
+        classificacao = "Peso ideal";
+    }else if (valorImc < 30){
+        classificacao = "Levemente acima do peso";
+    }else{
+        classificacao = "Obesidade";
+    }
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} , sua classificação é ${classificacao}`;
+}
 calcular.addEventListener('click',imc);
